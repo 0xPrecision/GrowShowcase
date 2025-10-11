b12548214f2285db01dbc72392bf60378975ea17
@nikita_akulionok <mikita0511@gmail.com>
2025-10-11 14:23:12 +0300

    ready

--- services/locale_repo.py
+++ services/locale_repo.py
@@ -12,23 +12,3 @@ class LocaleRepo:
     async def set(user_id: int, locale: str) -> None:
         await User.get_or_create(id=user_id, defaults={"full_name": "", "address": ""})
         await UserLocale.update_or_create(defaults={"locale": locale}, user_id=user_id)
-
-
-# from __future__ import annotations
-#
-# from typing import Optional
-#
-# from database.models import User, UserLocale
-#
-#
-# class LocaleRepo:
-#     @staticmethod
-#     async def get(user_id: int) -> Optional[str]:
-#         row = await UserLocale.get_or_none(user_id=user_id)
-#         return row.locale if row else None
-#
-#     @staticmethod
-#     async def set(user_id: int, locale: str) -> None:
-#         # гарантируем наличие User (если регистрируешь пользователей отдельно — можешь убрать это)
-#         await User.get_or_create(id=user_id, defaults={"full_name": "", "address": ""})
-#         await UserLocale.update_or_create(defaults={"locale": locale}, user_id=user_id)
